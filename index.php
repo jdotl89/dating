646bf056dae6aa9c4ddee2573534341c8f7a3f84
John Laygo <jlaygo@mail.greenriver.edu>
2020-05-10 16:59:48 -0400

    routed pages in views folder and added .htaccess file

--- index.php
+++ index.php
@@ -10,11 +10,45 @@ require_once('vendor/autoload.php');
 $f3 = Base::instance();
 
 // define a default route
-$f3->route('GET /', function(){
+$f3->route('GET /', function() {
     //echo '<h1>Hello world!</h1>';
 
     $view = new Template();
     echo $view->render('views/home.html');
+});
+
+// personal
+$f3->route('GET|POST /personal', function($f3) {
+
+    //$f3->reroute('/profile');
+
+    $view = new Template();
+    echo $view->render('views/personal.html');
+});
+
+// profile
+$f3->route('GET|POST /profile', function() {
+
+    $view = new Template();
+    echo $view->render('views/profile.html');
+});
+
+// interest
+$f3->route('GET|POST /interests', function() {
+
+    $view = new Template();
+    echo $view->render('views/interests.html');
+});
+
+// summary
+$f3->route('GET|POST /summary', function() {
+    //var_dump($_SESSION);
+    //echo "Thank You!";
+    //echo "<p>" . $_SESSION['pet'] . $_SESSION['pets'] . "</p>";
+
+    $view = new Template();
+    echo $view->render('views/summary.html');
+    //session_destroy();
 });
 
 // run fat free
