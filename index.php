35bacc18afb4a594b079db8846cc8c22a873e296
John Laygo <jlaygo@mail.greenriver.edu>
2020-05-12 22:46:44 -0400

    created model folder with data layer updated personal profile interests and started session array in index.php

--- index.php
+++ index.php
@@ -3,8 +3,12 @@
 ini_set('display_errors',1);
 error_reporting(E_ALL);
 
+//Start a session
+session_start();
+
 // require the autoload file
 require_once('vendor/autoload.php');
+require_once("model/data-layer.php");
 
 // create an instance of the base class
 $f3 = Base::instance();
@@ -21,34 +25,83 @@ $f3->route('GET /', function() {
 $f3->route('GET|POST /personal', function($f3) {
 
     //$f3->reroute('/profile');
+    //If the form has been submitted
+    if($_SERVER['REQUEST_METHOD'] == 'POST') {
+        var_dump($_POST);
+
+        //Store the data in the session array
+        $_SESSION['first'] = $_POST['first'];
+        $_SESSION['last'] = $_POST['last'];
+        $_SESSION['age'] = $_POST['age'];
+        $_SESSION['gender'] = $_POST['gender'];
+        $_SESSION['phone'] = $_POST['phone'];
+        $_SESSION['petName'] = $_POST['petName'];
+        $_SESSION['petAge'] = $_POST['petAge'];
+        $_SESSION['pGender'] = $_POST['pGender'];
+
+        //Redirect to Order 2 page
+        $f3->reroute('/profile');
+    }
 
     $view = new Template();
     echo $view->render('views/personal.html');
 });
 
 // profile
-$f3->route('GET|POST /profile', function() {
+$f3->route('GET|POST /profile', function($f3) {
+
+    //If the form has been submitted
+    if($_SERVER['REQUEST_METHOD'] == 'POST') {
+        var_dump($_POST);
+
+        //Store the data in the session array
+        $_SESSION['email'] = $_POST['email'];
+        $_SESSION['state'] = $_POST['state'];
+        $_SESSION['seek'] = $_POST['seek'];
+        $_SESSION['bio'] = $_POST['bio'];
+
+        //Redirect to Order 2 page
+        $f3->reroute('/interests');
+    }
 
     $view = new Template();
     echo $view->render('views/profile.html');
 });
 
 // interest
-$f3->route('GET|POST /interests', function() {
+$f3->route('GET|POST /interests', function($f3) {
+
+    $indoor = getInDoor();
+    $outdoor = getOutDoor();
+
+    //If the form has been submitted
+    if($_SERVER['REQUEST_METHOD'] == 'POST') {
+        var_dump($_POST);
+
+        //Store the data in the session array
+        $_SESSION['indoor'] = $_POST['indoor'];
+        $_SESSION['outdoor'] = $_POST['outdoor'];
+
+        //Redirect to Order 2 page
+        $f3->reroute('/summary');
+    }
+
+    $f3->set('indoor', $indoor);
+    $f3->set('outdoor', $outdoor);
 
     $view = new Template();
     echo $view->render('views/interests.html');
 });
 
 // summary
-$f3->route('GET|POST /summary', function() {
-    //var_dump($_SESSION);
+$f3->route('GET /summary', function() {
+    var_dump($_SESSION);
     //echo "Thank You!";
     //echo "<p>" . $_SESSION['pet'] . $_SESSION['pets'] . "</p>";
 
     $view = new Template();
     echo $view->render('views/summary.html');
-    //session_destroy();
+    session_destroy();
 });
 
 // run fat free
---
+++ model/data-layer.php
@@ -0,0 +1,13 @@
+<?php
+function getInDoor()
+{
+    $indoor = array("sleep", "eat", "catch", "toys", "climbing", "bathing", "staying", "licking");
+    return $indoor;
+}
+
+function getOutDoor()
+{
+    $outdoor = array("catch", "walking", "swimming", "toys", "climbing", "chasing", "wondering", "sniffing");
+    return $outdoor;
+}
+
