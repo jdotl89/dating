1ce655fa56eb30ef86e17c7a8fc731111cb4ea80
John Laygo <jlaygo@mail.greenriver.edu>
2020-05-31 03:43:27 -0400

    initialized controller.php file

---
+++ controllers/controller.php
@@ -0,0 +1,29 @@
+<?php
+
+class controller
+{
+    private $_f3; //router
+    private $_validator; //validation object
+
+    /**
+     * Controller constructor.
+     * @param $f3
+     * @param $validator
+     */
+    public function __construct($f3, $validator)
+    {
+        $this->_f3 = $f3;
+        $this->_validator = $validator;
+    }
+
+    /**
+     * Display the default route
+     */
+    public function home()
+    {
+        //echo '<h1>Welcome to my Food Page</h1>';
+
+        $view = new Template();
+        echo $view->render('views/home.html');
+    }
+}
